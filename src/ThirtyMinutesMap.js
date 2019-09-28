d73078efffc6cab0a23563e9fc1475ca67c9c716
Vincent Långström <vincent.langstrom@gmail.com>
2019-09-28 16:41:58 +0300

    update map url

--- src/ThirtyMinutesMap.js
+++ src/ThirtyMinutesMap.js
@@ -31,7 +31,7 @@ class ThirtyMinutesMap extends React.Component {
         <Map center={position} zoom={16} style={windowSizing()}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            url="https://cdn.digitransit.fi/map/v1/hsl-map-256/{z}/{x}/{y}.png"
           />
           <Marker position={position}>
             <Popup>
