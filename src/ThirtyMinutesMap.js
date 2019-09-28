2584e1f789d2d33857748e9651c29a10624e02bc
Antti <antti@lamia.fi>
2019-09-29 02:04:36 +0300

    Use new components in map

--- src/ThirtyMinutesMap.js
+++ src/ThirtyMinutesMap.js
@@ -7,71 +7,14 @@ import {
   Popup,
   Circle,
 } from "react-leaflet";
-import Leaflet from 'leaflet'
 import L from 'leaflet'
-import stationIcon from './icon/citybike.svg'
-import tooFarIcon from './icon/notgonnamakeit.svg'
-import locationIcon from './icon/youarehere.svg'
+import StationsList from './components/StationsList';
+import markerIcon from './leaflet/markerIcon';
 
 const windowSizing = () => ({
     height: `100vh`,
     width: `100vw`
-  });
-
-  export const pointerIcon = new L.Icon({
-    iconUrl: require('./icon/citybike.svg'),
-    iconRetinaUrl: require('./icon/citybike.svg'),
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 45],
-  })
-  export const notPointerIcon = new L.Icon({
-    iconUrl: require('./icon/citybike.svg'),
-    iconRetinaUrl: require('./icon/notgonnamakeit.svg'),
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 45],
-  })
-
-  const MyPopupMarker = ({ station }) => {
-
-    const icon = station.isInside 
-      ? pointerIcon
-      : notPointerIcon;
-
-    return (
-    <Marker position={[station.y, station.x]} icon={icon}>
-      <Popup >{station.name}</Popup>s
-    </Marker>
-    
-  )}
-  
-  class MyMarkersList extends React.Component {
-    componentDidUpdate(prevProps, prevState) {
-      if (prevProps.stations !== this.props.stations) {
-        return true;
-      }
-    }
-  
-    shouldComponentUpdate(nextProps, nextState) {
-      if (nextProps.stations !== this.props.stations) {
-        return true;
-      }
-    }
-
-    render() {
-      console.log("My marker list", this.props.stations)
-   
-      if(this.props.stations.length > 0) {
-        const items = this.props.stations.map(station => (
-          <MyPopupMarker key={station.id} station={station} />
-        ))
-        return <Fragment>{items}</Fragment>
-      }
-      return <div>Empty</div>
-    }
-  } 
-    
+});    
   
 class ThirtyMinutesMap extends React.Component {
 
@@ -89,32 +32,21 @@ class ThirtyMinutesMap extends React.Component {
 
   stationsInsideCircle = (stations, center) => {
     const centerLatLng = new L.latLng(center);
-    return stations.filter(station => {
-      return centerLatLng.distanceTo([station.y, station.x]) < 2000;
-    });
-  }
 
-  // lat y
-  // lng x
-  insideCircle(x, y, center_x, center_y, radius) {
-    return Math.sqrt(x - center_x) + Math.sqrt(y - center_y) <= Math.sqrt(radius);
+    return stations.map(station => {
+      station.isInside = (centerLatLng.distanceTo([station.y, station.x]) < 2000)
+        ? true
+        : false;
+
+      return station;
+    });
   }
 
   render() {
-      // @todo: somewhere else
-     const locationMarkerIcon = Leaflet.icon({
-      iconUrl: locationIcon,
-      iconSize: [35, 35],
-      iconAnchor: [15.5, 15.5],
-      popupAnchor: [-3, -76],
-      shadowSize: [68, 95],
-      shadowAnchor: [22, 94]
-    });
-
+    const locationMarkerIcon = markerIcon(require('./icon/map-marker.svg'));
     const position = [this.props.lat, this.props.lng];
-    const {stations} = this.props.stations;
-    
-    // Latitude: 1 deg = 110.574 km
+    const { area } = this.props;
+    const {stations} = this.props;
 
     return (
         <Map center={position} zoom={13} style={windowSizing()}>
@@ -122,11 +54,11 @@ class ThirtyMinutesMap extends React.Component {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://cdn.digitransit.fi/map/v1/hsl-map-256/{z}/{x}/{y}.png"
           />
-          {stations && <MyMarkersList stations={this.stationsInsideCircle(stations, position)}/>}
+          {stations && <StationsList stations={this.stationsInsideCircle(stations, position)}/>}
           <Marker position={position} icon={locationMarkerIcon}>
             <Popup>You are here</Popup>
           </Marker>
-          <Circle center={position} radius={2000}></Circle>
+          <Circle center={position} radius={3000}></Circle>
         </Map>
       );
   }
