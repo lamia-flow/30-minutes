4c6e0305c48957554b490f77d2913890f7d14931
majkrzak <petrol.91@gmail.com>
2019-09-30 15:03:38 +0000

    Deploy

---
+++ precache-manifest.f1c19028c2c82ec6fc1c9438fa1dac9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd65bf7d4263bd3a7447a3c40f9779f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e3219ba277dbb4b92fe",
+    "url": "/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "14cfa2f4d33fe351adc2",
+    "url": "/static/css/main.754f6db8.chunk.css"
+  },
+  {
+    "revision": "0e3219ba277dbb4b92fe",
+    "url": "/static/js/2.efb441a0.chunk.js"
+  },
+  {
+    "revision": "14cfa2f4d33fe351adc2",
+    "url": "/static/js/main.6a4a41ad.chunk.js"
+  },
+  {
+    "revision": "93b914fefabb11d7e958",
+    "url": "/static/js/runtime-main.20ca175d.js"
+  },
+  {
+    "revision": "36bcc6d28c194cdc08dd6316f1feed75",
+    "url": "/static/media/citybike.36bcc6d2.svg"
+  },
+  {
+    "revision": "cb5634bc4f3b56418f8b9822a678a426",
+    "url": "/static/media/notgonnamakeit.cb5634bc.svg"
+  },
+  {
+    "revision": "eb9a9cc8ba8805c581dc3ca87bc14c06",
+    "url": "/static/media/youarehere.eb9a9cc8.svg"
+  }
+]);
